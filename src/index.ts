990ac48b362e0b689c9da591c0af13e05c673d41
Marcos9868 <marcos_m_ferreira@outlook.com>
2025-08-21 20:14:11 -0300

    Feat: index
    Add simple express structure to create server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello world")
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+const server = express();
+dotenv.config();
+
+server.use(express.json());
+server.get("/", (req: Request, res: Response) => {
+  res.send("Hello world")
+});
+server.listen(process.env.PORT, () => console.log("Server running"));
