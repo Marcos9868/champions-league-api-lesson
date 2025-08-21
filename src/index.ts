39cfdd02c2dc3fef0e4a8bb1e7c355686e07a71f
Marcos9868 <marcos_m_ferreira@outlook.com>
2025-08-21 20:36:56 -0300

    Feat: server
    Add players route on server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { createApp } from "./app";
+import playersRoute from "./routes/PlayersRoute";
 
 const app = createApp();
+
+app.use(playersRoute);
 
 app.listen(process.env.PORT, () => console.log("Server running"))
